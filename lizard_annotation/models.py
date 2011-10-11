c010e9fe1b8d9a5af4879a528575c8a1e51a2066
Alexandr Seleznev <a.seleznev@hotmail.nl>
2011-10-11 14:15:09 +0200

    Added Annotation model.

--- lizard_annotation/models.py
+++ lizard_annotation/models.py
@@ -1,4 +1,55 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 # from django.db import models
 
-# Create your models here.
+import logging
+import datetime
+from django.db import models
+
+from django_mongokit import connection
+from django_mongokit.document import DjangoDocument
+
+class Annotation(DjangoDocument):
+
+    structure = {
+       'title': unicode,
+       # 'status': unicode,
+       # 'annotation_type': unicode,
+       # 'user_creator': unicode,
+       # 'user_midifier': unicode,
+       # 'dt_creation': datetime.datetime,
+       # 'dt_modification': datetime.datetime,
+       # 'relation_objects': {'name': unicode,
+       #                      'id': unicode},
+     }
+
+connection.register([Annotation])
+
+
+class AnnotationCategory(DjangoDocument):
+    structure = {
+        'category': unicode,
+        }
+
+    required_fields = ['category']
+
+connection.register([AnnotationCategory])
+
+
+class AnnotationType(DjangoDocument):
+    structure = {
+        'type': unicode,
+        }
+
+    required_fields = ['type']
+
+connection.register([AnnotationType])
+
+
+class AnnotationStatus(DjangoDocument):
+    structure = {
+        'status': unicode,
+        }
+
+    required_fields = ['status']
+
+connection.register([AnnotationStatus])
