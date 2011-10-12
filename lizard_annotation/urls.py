16211fb6850326c90b4946e85116d265d55cabef
= <arjan.verkerk@nelen-schuurmans.nl>
2011-10-12 13:39:17 +0200

    Added some views, forms and templates.

--- lizard_annotation/urls.py
+++ lizard_annotation/urls.py
@@ -5,12 +5,20 @@ from django.conf.urls.defaults import url
 from django.contrib import admin
 
 from lizard_ui.urls import debugmode_urlpatterns
+from lizard_annotation.views import AboutView
+from lizard_annotation.views import DetailView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     (r'^admin/', include(admin.site.urls)),
+    url(r'^about/',
+        AboutView.as_view(),
+        name='about_view'),
+    url(r'^detail/',
+        DetailView.as_view(),
+        name='detail_view'),
     # url(r'^something/',
     #     direct.import.views.some_method,
     #     name="name_it"),
--- lizard_annotation/views.py
+++ lizard_annotation/views.py
@@ -1,3 +1,17 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 
 # Create your views here.
+
+from django.views.generic import TemplateView
+from django.views.generic import FormView
+
+from lizard_annotation.forms import AnnotationForm
+
+class AboutView(TemplateView):
+    template_name = 'lizard_annotation/about.html'
+
+class DetailView(FormView):
+    
+    template_name = 'lizard_annotation/annotation_detail.html'
+    form_class = AnnotationForm
+    
