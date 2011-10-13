9484f599b6a461c376fe1a6dead8b071dd96abd0
= <arjan.verkerk@nelen-schuurmans.nl>
2011-10-13 16:32:43 +0200

    Added status choice field and validation to form.

--- lizard_annotation/forms.py
+++ lizard_annotation/forms.py
@@ -1,6 +1,25 @@
 
 from django import forms
 from models import Annotation
+from models import AnnotationStatus
+
+STATUS = (
+    ('New', 'New'),
+    ('Old', 'Old'),
+)
 
 class AnnotationForm(forms.Form):
+    """Form for editing of annotations."""
     title = forms.CharField()
+
+    status = forms.ChoiceField(
+        choices=STATUS,
+    )
+
+    def clean_status(self):
+        """Return the status object."""
+        data = self.cleaned_data['status']
+        data = AnnotationStatus.objects.get(status=data)
+        return data
+        
+
