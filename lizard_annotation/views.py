8c18c31d7f7a7ffc1500db78e75dcc1243b81b1a
= <arjan.verkerk@nelen-schuurmans.nl>
2011-10-18 09:51:31 +0200

    wip on views.

--- lizard_annotation/views.py
+++ lizard_annotation/views.py
@@ -4,21 +4,32 @@
 
 from django.views.generic import TemplateView
 from django.views.generic import FormView
+from django.utils.translation import pgettext
+from django.utils.translation import ugettext
 from lizard_annotation.forms import AnnotationForm
 from lizard_annotation.models import Annotation
 from lizard_annotation.models import AnnotationStatus
-
 
 class AnnotationEditView(FormView):
 
     template_name = 'lizard_annotation/annotation_form.html'
     form_class = AnnotationForm
-    # TODO Add a nice succes page
+    # TODO Add a nice success page
     success_url = '.'
 
     def form_valid(self, form):
         Annotation(**form.cleaned_data).save()
         return super(FormView, self).form_valid(form)
 
-    
+    def get_context_data(self, **kwargs):
+        """ Add extra label context to the context data."""
+        context = super(AnnotationEditView, self).get_context_data(**kwargs)
+        
+        kwargs.update({
+            'label': {
+                'save': pgettext(__name__, u'Save'),
+                'load': ugettext(u'Load'),
+            },       
+        })
 
+        return kwargs
