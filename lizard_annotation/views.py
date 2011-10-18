2233ac80dd6d91b85928c8a58c361a4e198d867c
Alexandr Seleznev <a.seleznev@hotmail.nl>
2011-10-18 09:58:02 +0200

    Added separate function to generate a context.

--- lizard_annotation/views.py
+++ lizard_annotation/views.py
@@ -3,12 +3,10 @@
 # Create your views here.
 
 from django.views.generic import FormView
-<<<<<<< HEAD
 from django.utils.translation import pgettext
 from django.utils.translation import ugettext
-=======
 from django.views.generic import TemplateView
->>>>>>> a3c31f11a55caeda5ffe5d5c09cdfbe6520b4cfd
+
 from lizard_annotation.forms import AnnotationForm
 from lizard_annotation.models import Annotation
 from lizard_annotation.models import AnnotationStatus
@@ -30,12 +28,12 @@ class AnnotationEditView(FormView):
     def get_context_data(self, **kwargs):
         """ Add extra label context to the context data."""
         context = super(AnnotationEditView, self).get_context_data(**kwargs)
-        
+
         kwargs.update({
             'label': {
                 'save': pgettext(__name__, u'Save'),
                 'load': ugettext(u'Load'),
-            },       
+            },
         })
 
         return kwargs
